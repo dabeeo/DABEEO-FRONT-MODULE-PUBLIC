8f5226a5b9570202d2014c1874ed745daa93d469
juseongjin-dabeeo <seongjin.ju@dabeeo.com>
2024-05-14 18:18:04 +0900

    [REACT-UI] 🐛 Fix: justify content 속성 값 수정

--- dabeeo-react-ui/src/components/Stack/Stack.types.ts
+++ dabeeo-react-ui/src/components/Stack/Stack.types.ts
@@ -1,14 +1,7 @@
 import {HTMLAttributes} from 'react';
 
 export type FlexDirection = 'row' | 'column' | 'row-reverse' | 'column-reverse' | undefined;
-export type JustifyContent =
-  | 'flex-start'
-  | 'flex-end'
-  | 'center'
-  | 'space-between'
-  | 'space-around'
-  | 'space-evenly'
-  | undefined;
+export type JustifyContent = 'start' | 'end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | undefined;
 export type AlignItems = 'stretch' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | undefined;
 
 export type NativeStackDivProps = HTMLAttributes<HTMLDivElement>;
